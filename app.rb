0b8a915cadecbd3d4d63281adbd6680f578b4aae
Wasif Khan <waasif.khaan@gmail.com>
2018-08-31 22:13:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,1 @@
-require 'sinatra/base'
 
-class App < Sinatra::Base
-
-  get '/newteam' do 
-    erb :newteam
-  end 
-  
-  post '/team' do
-    @team_name = params[:Team_name]
-    @coach = params[:coach]
-    @point_guard=params[:Point_guard]
-    @shooting_guard=params[:Shooting_guard]
-    @small_forward=params[:Small_forward]
-    @power_forward=params[:Power_forward]
-    @center=params[:Center]
-    erb :team
-  end
-end 
